1631b75cbc289d3e4be461c01d641f5beeb5d277
jacob <jacob@dyn3219-120.wlan.ic.ac.uk>
2020-10-27 12:15:10 +0000

    Changed loop value, measured time using oscilloscope

--- main.s
+++ main.s
@@ -9,15 +9,19 @@ main:
 
 	org	0x100		    ; Main code starts here at address 0x100
 start:
+	movlw	0xff
+	movwf	TRISD, A	    ; Port D all inputs
+	movff	PORTD, 0x05	    ; Saves value read by PORTD into 0x05
 	movlw 	0x0
-	movwf	TRISB, A	    ; Port C all outputs
+	movwf	TRISC, A	    ; Port C all outputs
+	clrf	LATC, A		    ; Clears the output
 	bra 	test
 loop:
-	movff 	0x06, PORTB
+	movff 	0x06, PORTC
 	incf 	0x06, W, A
 test:
-	movwf	0x06, A	    ; Test for end of loop condition
-	movlw 	0x63
+	movwf	0x06, A		    ; Test for end of loop condition
+	movf 	0x05, W
 	cpfsgt 	0x06, A
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
